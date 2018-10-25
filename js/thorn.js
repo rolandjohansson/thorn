ed91be33f8b210c7100f2f376d2ac7afd6a95795
rolandjohansson <hello@rolandjohansson.se>
2018-10-25 23:33:15 +0200

    progressbars added

--- js/thorn.js
+++ js/thorn.js
@@ -1,75 +1,207 @@
-var config = 
-  {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    parent: "graphics",
-    pixelArt: true,
-    scene: {
-      preload: preload,
-      create: create,
-      update: update
-    }
+/*
+    Thorn2.js
+    Author: Roland Johansson, October 2018
+    http://www.rolandjohansson.se/
+*/
+
+// TODO: Create setPlayerName function
+// TODO: Local store player highscore
+
+const gConsoleOutput = document.querySelector("#console");
+const gHealthBars = document.querySelectorAll(".healthbar");
+const gPlayerNameOutput = document.querySelector("#player-one-name");
+const gFoeNameOutput = document.querySelector("#player-two-name");
+const gRollButton = document.querySelector("#roll");
+
+const gPlayerDefaultName = "Brave";
+let gPlayer = createPlayer(gPlayerDefaultName);
+let gFoe = createFoe();
+
+/* ================= OBJECTS ================= */
+
+function createPlayer(playerName, hitPointsMax) {
+  let player = {
+    name: playerName,
+    visible: true,
+    hitPointsMax: 100,
+    hitPontsNow: 100,
+    pairMultiplier: 2,
+    // armor: "0",
+    basicDamage: 1,
+    luck: 0
+  };
+  return player;
+}
+
+function createFoe() {
+  const foeNames = [
+    "Grognar",
+    "Blurb",
+    "Snortiborp",
+    "Count Barfalot",
+    "Sproknaar"
+  ];
+  const foeName = foeNames[getRandomInt(0, foeNames.length - 1)];
+  const player = createPlayer(foeName);
+  player.hitPointsMax = getRandomInt(75, 125);
+  player.hitPointsNow = player.hitPointsMax;
+  return player;
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/* ================= UI ================= */
+
+function updateHealthBars(p1, p2) {
+  gHealthBars.forEach(healthBar => {
+    healthBar.setAttribute("max", gPlayer.hitPointsMax);
+    healthBar.setAttribute("value", gPlayer.hitPontsNow);
+  });
+}
+
+function updatePlayerNames() {
+  gPlayerNameOutput.innerHTML = gPlayer.name;
+  gFoeNameOutput.innerHTML = gFoe.name;
+}
+
+function writeToConsole(content) {
+  var element = document.createElement("p");
+  var txt = document.createTextNode(content);
+  element.innerHTML = txt.nodeValue;
+  gConsoleOutput.appendChild(element);
+  gConsoleOutput.scrollTop = gConsoleOutput.scrollHeight;
+}
+
+function fightRollOutput(attackerName, defenderName, result) {
+  writeToConsole("Hit! " + defenderName + " took <span class='text-danger'>" + result.actualDamage + "</span> damage");
+
+  // + attackerName +" rolled " + result.attackerDiceSum + " (" + result.attackerDie1 + " + " + result.attackerDie2  + "). " + defenderName + " rolled " + result.defenderDie1 + ". Damage: " + result.actualDamage + " (" + result.attackerDiceSum + " - " + result.defenderDie1)
+}
+
+/* ================= GAME ENGINE ================= */
+
+function startGame() {
+  updatePlayerNames();
+  updateHealthBars();
+  writeToConsole("Oh no! Our brave adventurer <span class='text-primary'>" + gPlayer.name + "</span> has encountered the infamous monster <span class='text-danger'>" + gFoe.name + "</span>. Time to roll the dice and fight!");
+  gRollButton.addEventListener("click", function () {
+    turn();
+  });
+}
+
+function fightRoll(attacker, defender) {
+  var outcome = {
+    attackerDie1: 0,
+    attackerDie2: 0,
+    defenderDie1: 0,
+    attackerDiceSum: 0,
+    actualDamage: 0
   };
 
-var player;
-var ground;
+  outcome.attackerDie1 = getRandomInt(1, 6);
+  outcome.attackerDie2 = getRandomInt(1, 6);
 
-var game = new Phaser.Game(config);
-
-function preload() 
-  {
-    this.load.image('bkg00', 'assets/backgrounds/forest00.png');
-    this.load.image('bkg01', 'assets/backgrounds/forest01.png');
-    this.load.image('bkg02', 'assets/backgrounds/forest02.png');
-    this.load.image('bkg03', 'assets/backgrounds/forest03.png');
-    this.load.image('bkg04', 'assets/backgrounds/forest04.png');
-    this.load.image('bkg05', 'assets/backgrounds/forest05.png');
-    this.load.image('bkg06', 'assets/backgrounds/forest06.png');
-    this.load.image('bkg07', 'assets/backgrounds/forest07.png');
-    this.load.image('bkg08', 'assets/backgrounds/forest08.png');
-    this.load.image('bkg09', 'assets/backgrounds/forest09.png');
-    this.load.image('bkg10', 'assets/backgrounds/forest10.png');
-    this.load.spritesheet('adventurer', 'assets/adventurer.png', { frameWidth: 50, frameHeight: 37 });
+  outcome.attackerDiceSum = outcome.attackerDie1 + outcome.attackerDie2;
+  if (outcome.attackerDie1 == outcome.attackerDie2) {
+    outcome.attackerDiceSum = outcome.attackerDiceSum * attacker.pairMultiplier;
   }
 
-function create() 
-  {
-    this.add.image(400, 204, 'bkg00');
-    this.add.image(400, 204, 'bkg01');
-    this.add.image(400, 204, 'bkg02');
-    this.add.image(400, 204, 'bkg03');
-    this.add.image(400, 204, 'bkg04');
-    this.add.image(400, 204, 'bkg05');
-    this.add.image(400, 204, 'bkg06');
-    this.add.image(400, 204, 'bkg07');
-    this.add.image(400, 204, 'bkg08');
-    this.add.image(400, 204, 'bkg09');
+  outcome.defenderDie1 = getRandomInt(1, 6);
 
-    player = this.add.sprite(100, 490, "adventurer").setScale(3);
-    
-    this.add.image(400, 204, 'bkg10');
+  outcome.actualDamage = outcome.attackerDiceSum - outcome.defenderDie1;
 
-    this.anims.create({
-      key: "idle",
-      frames: this.anims.generateFrameNumbers("adventurer", { start: 38, end: 41 }),
-      frameRate: 3
-    });
-
-    this.anims.create({
-      key: "run-right",
-      frames: this.anims.generateFrameNumbers("adventurer", { start: 8, end: 13 }),
-      frameRate: 10
-    });
-
-    this.anims.create({
-      key: "attack1",
-      frames: this.anims.generateFrameNumbers("adventurer", { start: 42, end: 46 }),
-      frameRate: 10
-    });
+  if (outcome.actualDamage < 0) {
+    outcome.actualDamage = 0;
   }
 
-function update()
-  {
-    player.anims.play("idle", true);
+  defender.hitPointsNow -= outcome.actualDamage;
+
+  if (defender.hitPointsNow < 0) {
+    defender.hitPointsNow = 0;
   }
+
+  return outcome;
+
+}
+
+function playerTurn() {
+  var result = fightRoll(gPlayer, gFoe);
+  fightRollOutput(gPlayer.name, gFoe.name, result)
+}
+
+function foeTurn() {
+  var result = fightRoll(gFoe, gPlayer);
+  fightRollOutput(gFoe.name, gPlayer.name, result)
+}
+
+function turn() {
+  playerTurn();
+  foeTurn();
+}
+
+startGame();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const gGRAPHICSAREA = document.querySelector(".graphics")
+// const gDICEIMAGESPATH = "assets/dice/"
+// var console = {};
+
+// console.log = function(text) {
+//   var element = document.createElement("div");
+//   var txt = document.createTextNode(text);
+//   element.appendChild(txt);
+//   gCONSOLEOUTPUT.appendChild(element);
+// }
+
+// function rollDice(numberOfDice) {
+//   if (document.querySelector(".dice")) {
+//     document.querySelectorAll('.dice').forEach(function(i) {
+//       i.remove()
+//     })
+//   }
+
+//   for (i = 0; i < numberOfDice; ++i) {
+//     const DIEROLL = Math.floor(Math.random() * 6) + 1;
+//     drawDice(DIEROLL);
+//   }
+// }
+
+// function drawDice(DIEROLL) {
+//   console.log("You rolled " + DIEROLL);
+//   gCONSOLEOUTPUT.scrollTop = gCONSOLEOUTPUT.scrollHeight;
+//   const DICE = document.createElement("img");
+//   DICE.setAttribute("src", gDICEIMAGESPATH + DIEROLL + ".svg");
+//   DICE.setAttribute("class", "dice");
+//   DICE.setAttribute("id", "die" + (i + 1));
+//   gGRAPHICSAREA.appendChild(DICE);
+// }
+
+// gROLLBTN.addEventListener("click", function() {
+//   rollDice(2);
+// });
